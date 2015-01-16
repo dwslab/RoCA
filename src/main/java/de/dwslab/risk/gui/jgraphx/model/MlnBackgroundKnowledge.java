c64f45b31c720b13df775b1e33e8b29c3882894c
Joerg Schoenfisch <joe776@users.noreply.github.com>
2015-01-16 17:32:38 +0100

    mln parsing

--- src/main/java/de/dwslab/risk/gui/jgraphx/model/MlnBackgroundKnowledge.java
+++ src/main/java/de/dwslab/risk/gui/jgraphx/model/MlnBackgroundKnowledge.java
@@ -1,69 +1,131 @@
 package de.dwslab.risk.gui.jgraphx.model;
 
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import com.googlecode.rockit.javaAPI.Model;
-import com.googlecode.rockit.parser.SyntaxReader;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import de.dwslab.risk.gui.exception.RoCAException;
 
 public class MlnBackgroundKnowledge implements BackgroundKnowledge {
 
-    public MlnBackgroundKnowledge(String mln, String evidence) {
+    private final Map<String, Predicate> predicates;
+    private final Map<String, List<String>> entities;
+    private final Map<String, Set<Literal>> groundings;
+
+    public MlnBackgroundKnowledge(Path mln, Path evidence) {
         try {
-            SyntaxReader reader = new SyntaxReader();
-            Model model = reader.getModel(mln, evidence);
-
-            model.getAllHiddenPredicates();
-            model.getAllObservedPredicates();
-            model.getAllTypes();
-            model.getFormulas();
-
+            predicates = parsePredicates(mln);
+            entities = parseEntities(evidence);
+            groundings = parseGroundings(evidence);
         } catch (Exception e) {
             throw new RoCAException("Cannot load background knowledge.", e);
         }
     }
 
-    private static Map<String, Predicate> getPredicates(String mln) throws Exception {
+    private Map<String, Predicate> parsePredicates(Path mln) throws IOException {
         Map<String, Predicate> map = new HashMap<>();
 
-        Files.lines(Paths.get(mln))
-                .filter(s -> !s.isEmpty() && !s.startsWith("//") && !s.contains("v")
-                        && !s.contains("\""))
-                .map(s -> s.trim())
-                .map(s -> (s.startsWith("*") ? s.substring(1) : s))
-                .forEach(
-                        p -> {
-                            if ((p.length() > 0) && !p.startsWith("//")
-                                    && (!p.contains("v") && !p.contains("\""))) {
-                                boolean observed = false;
-                                if (p.startsWith("*")) {
-                                    observed = true;
-                                    p = p.substring(1);
-                                }
-
-                                String name = p.substring(0, p.indexOf("(")).trim();
-
-                                List<String> types = new ArrayList<>();
-                                Pattern pattern = Pattern.compile("([^\\s(),]+)");
-                                Matcher matcher = pattern.matcher(p.substring(p.indexOf("(")));
-                                while (matcher.find()) {
-                                    types.add(matcher.group(1));
-                                }
-
-                                Predicate pred = new Predicate(observed, name, types);
-                                map.put(name, pred);
-                            }
-
-                        });
+        Files.lines(mln)
+                .filter(str -> !str.isEmpty() && !str.startsWith("//") && !str.contains("v")
+                        && !str.contains("\""))
+                .map(String::trim)
+                .map(str -> (str.startsWith("*") ? str.substring(1) : str))
+                .forEach(str -> {
+                    String name = str.substring(0, str.indexOf('('));
+                    String[] types = str.substring(str.indexOf('(') + 1, str.indexOf(')'))
+                            .split("\\s*,\\s*");
+                    Predicate pred = new Predicate(false, name, Arrays.asList(types));
+                    map.put(name, pred);
+                });
 
         return map;
     }
+
+    private Map<String, List<String>> parseEntities(Path evidence) throws IOException {
+        Map<String, Set<String>> entities = new HashMap<>();
+
+        Files.lines(evidence)
+                .filter(str -> !str.isEmpty() && !str.startsWith("//"))
+                .map(String::trim)
+                .map(str -> (str.startsWith("!") ? str.substring(1) : str))
+                .forEach(str -> {
+                    String predicate = str.substring(0, str.indexOf('('));
+                    String entitiesString = str.substring(str.indexOf('(') + 1, str.indexOf(')'));
+                    String[] entititesArr = entitiesString.split("\\s*,\\s*");
+
+                    List<String> types = predicates.get(predicate).getTypes();
+                    for (int i = 0; i < types.size(); i++) {
+                        String type = types.get(i);
+                        String entity = entititesArr[i];
+                        Set<String> entitySet = entities.get(type);
+                        if (entitySet == null) {
+                            entitySet = new HashSet<>();
+                            entities.put(type, entitySet);
+                        }
+                        entitySet.add(entity);
+                    }
+                });
+
+        // string -> list instead of string -> set
+        Map<String, List<String>> entities2 = new HashMap<>();
+        for (Entry<String, Set<String>> entry : entities.entrySet()) {
+            entities2.put(entry.getKey(), new ArrayList<>(entry.getValue()));
+        }
+
+        return entities2;
+    }
+
+    private static Map<String, Set<Literal>> parseGroundings(Path evidence) throws Exception {
+        Map<String, Set<Literal>> groundings = new HashMap<>();
+
+        Files.lines(evidence)
+                .filter(str -> !str.isEmpty() && !str.startsWith("//"))
+                .map(String::trim)
+                .map(str -> (str.startsWith("!") ? str.substring(1) : str))
+                .forEach(str -> {
+                    String predicate = str.substring(0, str.indexOf('('));
+                    String entitiesString = str.substring(str.indexOf('(') + 1, str.indexOf(')'));
+                    String[] entititesArr = entitiesString.split("\\s*,\\s*");
+
+                    Literal literal = new Literal(false, predicate, Arrays.asList(entititesArr));
+                    Set<Literal> groundingsSet = groundings.get(predicate);
+                    if (groundingsSet == null) {
+                        groundingsSet = new HashSet<>();
+                        groundings.put(predicate, groundingsSet);
+                    }
+                    groundingsSet.add(literal);
+                });
+
+        return groundings;
+    }
+
+    /**
+     * @return the predicates
+     */
+    public Map<String, Predicate> getPredicates() {
+        return predicates;
+    }
+
+    /**
+     * @return the entities
+     */
+    public Map<String, List<String>> getEntities() {
+        return entities;
+    }
+
+    /**
+     * @return the groundings
+     */
+    public Map<String, Set<Literal>> getGroundings() {
+        return groundings;
+    }
+
 }
