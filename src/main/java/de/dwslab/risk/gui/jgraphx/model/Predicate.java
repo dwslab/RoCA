bdc4c2f2fb0d98d2876c60b52273f2d91992840f
Joerg Schoenfisch <joe776@users.noreply.github.com>
2015-01-29 17:44:14 +0100

    Generate hashCode and equals

--- src/main/java/de/dwslab/risk/gui/jgraphx/model/Predicate.java
+++ src/main/java/de/dwslab/risk/gui/jgraphx/model/Predicate.java
@@ -31,6 +31,40 @@ public class Predicate {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + (negated ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Predicate other = (Predicate) obj;
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (negated != other.negated) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public String toString() {
         StringBuilder pred = new StringBuilder();
         if (negated) {
