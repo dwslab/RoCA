5f0c756c0599858d11558370b7d32d710626f568
Joerg Schoenfisch <joe776@users.noreply.github.com>
2015-02-04 14:30:48 +0100

    Add more specific classes to the model.
    Issue 1: https://bitbucket.org/joe776/risk-impl-roca/issue/1/

---
+++ src/main/java/de/dwslab/risk/gui/model/Entity.java
@@ -0,0 +1,73 @@
+package de.dwslab.risk.gui.model;
+
+public class Entity {
+
+    private final String name;
+    private final boolean offline;
+    private final Type type;
+
+    public Entity(String name, Type type) {
+        this(name, type, false);
+    }
+
+    public Entity(String name, Type type, boolean offline) {
+        this.name = name;
+        this.type = type;
+        this.offline = offline;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public boolean isOffline() {
+        return offline;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + (offline ? 1231 : 1237);
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Entity other = (Entity) obj;
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (offline != other.offline) {
+            return false;
+        }
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/de/dwslab/risk/gui/model/Type.java
@@ -0,0 +1,45 @@
+package de.dwslab.risk.gui.model;
+
+public class Type {
+
+    private final String name;
+
+    public Type(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Type other = (Type) obj;
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+}
