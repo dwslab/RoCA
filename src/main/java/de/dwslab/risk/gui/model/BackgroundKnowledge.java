5fd6dda51e2fb09b822a815ea699d6a67b9b687d
Joerg Schoenfisch <joe776@users.noreply.github.com>
2015-02-09 13:44:43 +0100

    Write the information in the background model to files.
    Issue 3: https://bitbucket.org/joe776/risk-impl-roca/issue/3/

--- src/main/java/de/dwslab/risk/gui/model/BackgroundKnowledge.java
+++ src/main/java/de/dwslab/risk/gui/model/BackgroundKnowledge.java
@@ -1,12 +1,21 @@
 package de.dwslab.risk.gui.model;
 
+import static org.jooq.lambda.Unchecked.consumer;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+
+import de.dwslab.risk.gui.exception.RoCAException;
 
 public interface BackgroundKnowledge {
 
@@ -46,6 +55,61 @@ public interface BackgroundKnowledge {
             }
         }
         return new AggregatedBackgroundKnowledge(predicates, types, entities, groundings);
+    }
+
+    public default void exportAsMln(Path mln, Path evidence) {
+        try (BufferedWriter mlnWriter = Files.newBufferedWriter(mln)) {
+            Map<String, Predicate> predicates = getPredicates();
+            for (Predicate predicate : predicates.values()) {
+                if (predicate.isObserved()) {
+                    mlnWriter.write('*');
+                }
+                mlnWriter.write(predicate.getName());
+                mlnWriter.write('(');
+                List<Type> types = predicate.getTypes();
+                for (int i = 0; i < types.size() - 1; i++) {
+                    mlnWriter.write(types.get(i).getName());
+                    mlnWriter.write(',');
+                }
+                mlnWriter.write(types.get(types.size() - 1).getName());
+                mlnWriter.write(')');
+                mlnWriter.newLine();
+            }
+
+            // TODO write formulas
+
+        } catch (IOException e) {
+            throw new RoCAException("Cannot write model file.", e);
+        }
+
+        try (BufferedWriter evidenceWriter = Files.newBufferedWriter(evidence)) {
+            Map<Predicate, Set<Grounding>> groundings = getGroundings();
+            groundings.values().stream()
+                    .flatMap(g -> g.stream())
+                    .forEach(consumer(g -> {
+                        Predicate predicate = g.getPredicate();
+                        if (predicate.isNegated()) {
+                            evidenceWriter.write('!');
+                        }
+                        evidenceWriter.write(predicate.getName());
+                        evidenceWriter.write('(');
+                        List<String> values = g.getValues();
+                        for (int i = 0; i < values.size() - 1; i++) {
+                            String value = values.get(i);
+                            evidenceWriter.write(value);
+                            evidenceWriter.write(',');
+                        }
+                        evidenceWriter.write(values.get(values.size() - 1));
+                        evidenceWriter.write(')');
+                        evidenceWriter.newLine();
+                    }));
+        } catch (IOException e) {
+            throw new RoCAException("Cannot write evidence file.", e);
+        }
+    }
+
+    public default void exportAsOntology(Path mln, Path owl) {
+        throw new RoCAException("Exporting as ontology not implemented.");
     }
 
     /**
