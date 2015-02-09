0d089a1dac5cd3db58228834be10172c4306ff54
Joerg Schoenfisch <joe776@users.noreply.github.com>
2015-02-09 14:15:04 +0100

    Add a simple test case for MLN export.
    Issue 3: https://bitbucket.org/joe776/risk-impl-roca/issue/3/

---
+++ src/test/java/de/dwslab/risk/gui/model/BackgroundKnowledgeTest.java
@@ -0,0 +1,25 @@
+package de.dwslab.risk.gui.model;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import de.dwslab.ai.util.Utils;
+
+public class BackgroundKnowledgeTest {
+
+    private static final Path mln = Paths.get("src/test/resources/test4_2.mln");
+    private static final Path evidence = Paths.get("src/test/resources/test4_2.db");
+
+    @Test
+    public void testExportAsMln() throws Exception {
+        BackgroundKnowledge knowledge = new MlnBackgroundKnowledge(mln, evidence);
+
+        Path tempMln = Utils.createTempPath("mln-", ".mln");
+        Path tempEvidence = Utils.createTempPath("evidence-", ".db");
+
+        knowledge.exportAsMln(tempMln, tempEvidence);
+    }
+
+}
