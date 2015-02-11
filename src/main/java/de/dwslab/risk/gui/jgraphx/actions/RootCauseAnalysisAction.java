52e4c61176ddb7202745555d4443f17118d14ba4
Joerg Schoenfisch <joe776@users.noreply.github.com>
2015-02-11 15:35:43 +0100

    Extend the MLN for the root cause analysis
    Issue 2: https://bitbucket.org/joe776/risk-impl-roca/issue/2/

--- src/main/java/de/dwslab/risk/gui/jgraphx/actions/RootCauseAnalysisAction.java
+++ src/main/java/de/dwslab/risk/gui/jgraphx/actions/RootCauseAnalysisAction.java
@@ -1,10 +1,13 @@
 package de.dwslab.risk.gui.jgraphx.actions;
 
 import static de.dwslab.ai.util.Utils.createTempFile;
+import static java.util.stream.Collectors.toSet;
+import static org.apache.logging.log4j.Level.INFO;
 
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.AbstractAction;
 import javax.swing.ProgressMonitor;
@@ -19,6 +22,9 @@ import com.googlecode.rockit.app.result.RockItResult;
 
 import de.dwslab.ai.riskmanagement.abduction.existential.ExistentialApi;
 import de.dwslab.risk.gui.RoCA;
+import de.dwslab.risk.gui.exception.RoCAException;
+import de.dwslab.risk.gui.model.Grounding;
+import de.dwslab.risk.gui.model.Predicate;
 
 public class RootCauseAnalysisAction extends AbstractAction {
 
@@ -37,10 +43,11 @@ public class RootCauseAnalysisAction extends AbstractAction {
         worker.execute();
     }
 
-    private static class RootCauseAnalysisWorker extends SwingWorker<Void, Void> {
+    private static class RootCauseAnalysisWorker extends SwingWorker<Set<Grounding>, Integer> {
 
         private final RoCA roca;
-        private ProgressMonitor monitor;
+        private final ProgressMonitor monitor;
+        private Exception e;
 
         public RootCauseAnalysisWorker(RoCA roca) {
             this.roca = roca;
@@ -52,33 +59,57 @@ public class RootCauseAnalysisAction extends AbstractAction {
         }
 
         @Override
-        protected Void doInBackground() throws Exception {
-            // Export the background knwoledge to temporary files
-            File mlnFile = createTempFile("mln-", ".mln");
-            File evidenceFile = createTempFile("evidence-", ".db");
-            roca.getBackgroundKnowledge().exportAsMln(mlnFile.toPath(), evidenceFile.toPath());
+        protected Set<Grounding> doInBackground() throws Exception {
+            try {
+                // Export the background knowledge to temporary files
+                logger.log(INFO, "exporting to temporary file");
+                monitor.setProgress(1);
+                monitor.setNote("Generating temporary MLN files...");
+                File mlnFile = createTempFile("mln-", ".mln");
+                File evidenceFile = createTempFile("evidence-", ".db");
+                roca.getBackgroundKnowledge().exportAsMln(mlnFile.toPath(), evidenceFile.toPath());
+                monitor.setProgress(10);
 
-            // Extend the MLN for abductive reasoning
-            ExistentialApi api = new ExistentialApi();
-            File mlnExtFile = createTempFile("mln-", ".mln");
-            File evidenceExtFile = createTempFile("evidence-", ".db");
-            api.existenitalApi(mlnFile.getAbsolutePath(), evidenceFile.getAbsolutePath(),
-                    mlnExtFile.getAbsolutePath(), evidenceExtFile.getAbsolutePath());
+                // Extend the MLN for abductive reasoning
+                logger.log(INFO, "extending MLN");
+                monitor.setNote("Extending MLN for root cause analysis...");
+                ExistentialApi api = new ExistentialApi();
+                File mlnExtFile = createTempFile("mln-", ".mln");
+                File evidenceExtFile = createTempFile("evidence-", ".db");
+                api.existentialApi(mlnFile.getAbsolutePath(), evidenceFile.getAbsolutePath(),
+                        mlnExtFile.getAbsolutePath(), evidenceExtFile.getAbsolutePath());
+                monitor.setProgress(20);
 
-            // Run RockIt
-            RockItAPI rockit = new RockItAPI("src/main/resources/rockit.properties");
-            List<RockItResult> mapState = rockit.doMapState(mlnExtFile.getAbsolutePath(),
-                    evidenceExtFile.getAbsolutePath());
+                // Run RockIt
+                logger.log(INFO, "running map inference");
+                monitor.setNote("Executing MAP inference...");
+                RockItAPI rockit = new RockItAPI("src/main/resources/rockit.properties");
+                List<RockItResult> mapState = rockit.doMapState(mlnExtFile.getAbsolutePath(),
+                        evidenceExtFile.getAbsolutePath());
+                monitor.setProgress(90);
 
-            // Process the result
-            mapState.forEach(m -> System.err.println(m));
+                // Process the result
+                logger.log(INFO, "processing result");
+                monitor.setNote("Processing MAP result...");
+                Set<Grounding> rootCause = mapState.stream()
+                        .filter(m -> "hasRisk".equals(m.getPredicate()))
+                        .map(m -> new Grounding(new Predicate(m.getPredicate()), m.getObjects()))
+                        .collect(toSet());
+                monitor.setProgress(100);
 
-            return null;
+                return rootCause;
+            } catch (Exception e) {
+                this.e = e;
+                throw e;
+            }
         }
 
         @Override
         protected void done() {
             monitor.close();
+            if (e != null) {
+                throw new RoCAException("Exception during root cause analysis", e);
+            }
         }
 
     }
