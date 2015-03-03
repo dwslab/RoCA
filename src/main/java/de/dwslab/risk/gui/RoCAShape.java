3a0a9c8062aaee3aaed24fcc7e61d56e9ac971a2
Joerg Schoenfisch <joe776@users.noreply.github.com>
2015-03-03 17:02:44 +0100

    Encode types in the shapes

--- src/main/java/de/dwslab/risk/gui/RoCAShape.java
+++ src/main/java/de/dwslab/risk/gui/RoCAShape.java
@@ -12,15 +12,15 @@ public enum RoCAShape implements RoCAIcons {
 
     SERVICE("Service", IMAGE_WRENCH,
             "icon;image=/com/mxgraph/examples/swing/images/wrench.png",
-            120, 120, Entity.create("New Service", newComponent("infra"))),
+            120, 120, Entity.create("New Service", newComponent("service"))),
 
     SERVER("Server", IMAGE_SERVER,
             "icon;image=/com/mxgraph/examples/swing/images/server.png",
-            120, 120, Entity.create("New Server", newComponent("infra"))),
+            120, 120, Entity.create("New Server", newComponent("server"))),
 
     PRINTER("Printer", IMAGE_PRINTER,
             "icon;image=/com/mxgraph/examples/swing/images/printer.png",
-            120, 120, Entity.create("New Printer", newComponent("infra"))),
+            120, 120, Entity.create("New Printer", newComponent("printer"))),
 
     COMPONENT("Component", ICON_RECTANGLE, null,
             160, 120, Entity.create("New Component", newComponent("infra"))),
